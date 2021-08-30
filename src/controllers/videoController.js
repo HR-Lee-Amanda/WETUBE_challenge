79d8614a5a2fdb555208e9c472a89ac1848e03d2
DESKTOP-OGF00CJ\Amanda <gpfla7946@gmail.com>
2021-08-30 14:51:45 +0900

    # 2021.08.30 # 6.28 까지
    Mongoose 사용법
    Schema => statics, middlewares
    MongoDB => STRONG regular expressions
    CRUD

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,20 +1,37 @@
 import Video from "../models/video";
 
 export const trending = async(req, res) => {
-    const videos = await Video.find({});
+    const videos = await Video.find({}).sort({createdAt : "descending"});
     return res.render("home", {pageTitle : "Home", videos}); // controller
 }
-export const watch = (req, res) => {
+export const watch = async(req, res) => {
     const id = req.params.id; // const {id} = req.params;
-    res.render("watch", {pageTitle : `Watch : `});
+    const video = await Video.findById(id);
+    if (video) 
+        return res.render("watch", {pageTitle : video.title, video});
+    else return res.render("404", {pageTitle : "Video Not Found"});
 }
-export const edit = (req, res) => {
+export const getEdit = async(req, res) => {
     const id = req.params.id;
-    res.render("edit", {pageTitle : `Edit : `});
+    console.log(req.params);
+    const video = await Video.findById(id);
+    if (!video) {
+       return res.render("404", {pageTitle : "Video Not Found"});
+    } 
+    console.log("Here");
+    return res.render("edit", {pageTitle : `Edit : ${video.title}`, video});
 }
-export const postEdit = (req, res) => {
+export const postEdit = async(req, res) => {
     const id = req.params.id;
-    const {title} = req.body;
+    const video = await Video.exists({_id : id});
+    const {title, description, hashtags} = req.body;
+    if (!video) {
+        return res.render("404", {pageTitle : "Video Not Found"});
+    } 
+    await Video.findByIdAndUpdate(id, {
+        title, description, 
+        hashtags : Video.formatHashtags(hashtags)
+    });
     return res.redirect(`/videos/${id}`);
 }
 export const getUpload = (req, res) => {
@@ -23,17 +40,13 @@ export const getUpload = (req, res) => {
 export const postUpload = async (req, res) => {
     // console.log(req.body);
     const {videoTitle, description, hashtags} = req.body;
-    await Video.create({ // document
-        title : videoTitle,
-        description,
-        createdAt : Date.now(),
-        hashtags : hashtags.split(",").map(word => `#${word}`),
-        meta : {
-            views : 0,
-            rating : 0
-        }
-    });
-    /* 위의 코드와 같다. (데이터베이스에 저장)
+    try {
+        await Video.create({ // document
+            title : videoTitle,
+            description,
+            hashtags : Video.formatHashtags(hashtags)
+        });
+        /* 위의 코드와 같다. (데이터베이스에 저장)
         const video = new Video({ // document
         title : videoTitle,
         description,
@@ -45,6 +58,26 @@ export const postUpload = async (req, res) => {
         }
     }); 
     await video.save(); */
+    } catch (error) {
+        return res.render("upload", {pageTitle: "Upload Video", errorMessage : error._message});
+    }
     
     return res.redirect("/");
+}
+export const remove = async (req, res) => {
+    const {id} = req.params;
+    await Video.findByIdAndDelete(id);
+    return res.redirect("/");
+}
+export const search = async (req, res) => {
+    const { keyword } = req.query;
+    let videos = [];
+    if (keyword) { // 실제로 검색한 경우
+        videos = await Video.find({
+            title : {
+                $regex: new RegExp(keyword, "i")
+            }
+        });
+    }
+    return res.render("search", {pageTitle :"Search", videos});
 }
--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,15 +1,13 @@
 import express from "express";
-import {watch, edit, getUpload, postUpload, remove, postEdit} from "../controllers/videoController";
+import {watch, getEdit, getUpload, postUpload, remove, postEdit} from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
 //videoRouter.get("/upload", upload); // 이게 id 보다 먼저 와야됨 id는 변수라서.
-videoRouter.get("/:id(\\d+)", watch); // id(\\d+) 이거는 정규식과 이름의 결합 ,, 이름 없으면 controller에서 req object 통해 불러올 수 없다.
+videoRouter.route("/upload").get(getUpload).post(postUpload);
+videoRouter.get("/:id([0-9a-f]{24})", watch); // id(\\d+) 이거는 정규식과 이름의 결합 ,, 이름 없으면 controller에서 req object 통해 불러올 수 없다.
 //videoRouter.route("/:id(\\d+)/edit").get(edit).post(postEdit); -> 아래 두 줄 통합
-videoRouter.post("/:id(\\d+)/edit", postEdit);
-videoRouter.get("/:id(\\d+)/edit", edit);
-//videoRouter.get("/:id(\\d+)/remove", remove);
-videoRouter.get("/upload", getUpload);
-videoRouter.post("/upload", postUpload);
-
+videoRouter.post("/:id([0-9a-f]{24})/edit", postEdit);
+videoRouter.get("/:id([0-9a-f]{24})/edit", getEdit);
+videoRouter.get("/:id([0-9a-f]{24})/delete", remove);
 export default videoRouter;
